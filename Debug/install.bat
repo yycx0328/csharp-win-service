a48f99901f42ea83ca019b2d1d09d11c31c4e6d9
yycx0328 <lyh_oracle@hotmail.com>
2017-07-07 12:44:54 +0800

    ignore file

--- Debug/install.bat
+++
@@ -1,3 +0,0 @@
-%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\InstallUtil WinService.exe
-Net Start WinService
-sc config WinService start= auto
--- Debug/uninstall.bat
+++
@@ -1,1 +0,0 @@
-%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\InstallUtil /u WinService.exe
